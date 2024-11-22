8e17c3e79c1dcd668c5ebffaf368d396cf0c4f37
John Webber <j.webber@roadsurfer.com>
2024-11-22 16:26:26 +0100

    Initial commit of new nflsurvivor repository

---
+++ src/main/java/com/webber/nflsurvivor/domain/Team.java
@@ -0,0 +1,8 @@
+package com.webber.nflsurvivor.domain;
+
+public class Team {
+
+    private String name;
+
+    private byte[] logo;
+}
---
+++ src/test/java/com/webber/nflsurvivor/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webber.nflsurvivor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
