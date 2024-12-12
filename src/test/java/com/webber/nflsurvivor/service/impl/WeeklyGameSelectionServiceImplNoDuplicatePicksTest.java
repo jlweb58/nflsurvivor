a4cb274d17946706073fc8c88738b383ab17f552
John Webber <jlweb58@gmail.com>
2024-12-12 14:34:03 +0100

    Added 2 additional test cases for the no duplicate picks business logic

--- src/test/java/com/webber/nflsurvivor/service/impl/WeeklyGameSelectionServiceImplNoDuplicatePicksTest.java
+++ src/test/java/com/webber/nflsurvivor/service/impl/WeeklyGameSelectionServiceImplNoDuplicatePicksTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import java.time.LocalDateTime;
 import java.time.ZonedDateTime;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.when;
 
@@ -15,7 +16,7 @@ public class WeeklyGameSelectionServiceImplNoDuplicatePicksTest extends Abstract
 
 
     @Test
-    public void testCanCreateTwoSelectionsWithDifferentWinners() throws Exception {
+    public void testCannotCreateTwoSelectionsWithSameWinner() throws Exception {
         when(dateTimeService.getCurrentDateTime()).thenReturn(ZonedDateTime.of(LocalDateTime.of(2024, 10, 10, 9, 0), berlinZone));
         Game selectedGame1 = gameService.create(new Game(team1, team2, 1, ZonedDateTime.now()));
         Game selectedGame2 = gameService.create(new Game(team2, team3, 2, ZonedDateTime.now()));
@@ -24,4 +25,30 @@ public class WeeklyGameSelectionServiceImplNoDuplicatePicksTest extends Abstract
             WeeklyGameSelection weeklyGameSelection2 = weeklyGameSelectionService.create(new WeeklyGameSelection(user1, team2, selectedGame2));
         });
     }
+
+    @Test
+    public void testCanCreateTwoSelectionsWithDifferentWinners() throws Exception {
+        when(dateTimeService.getCurrentDateTime()).thenReturn(ZonedDateTime.of(LocalDateTime.of(2024, 10, 10, 9, 0), berlinZone));
+        Game selectedGame1 = gameService.create(new Game(team1, team2, 1, ZonedDateTime.now()));
+        Game selectedGame2 = gameService.create(new Game(team2, team3, 2, ZonedDateTime.now()));
+        WeeklyGameSelection weeklyGameSelection1 = weeklyGameSelectionService.create(new WeeklyGameSelection(user1, team2, selectedGame1));
+        WeeklyGameSelection weeklyGameSelection2 = weeklyGameSelectionService.create(new WeeklyGameSelection(user1, team3, selectedGame2));
+        assertNotNull(weeklyGameSelection1);
+        assertNotNull(weeklyGameSelection2);
+    }
+
+    @Test
+    public void testCanCreateThreeSelectionsWithDifferentWinners() throws Exception {
+        when(dateTimeService.getCurrentDateTime()).thenReturn(ZonedDateTime.of(LocalDateTime.of(2024, 10, 10, 9, 0), berlinZone));
+        Game selectedGame1 = gameService.create(new Game(team1, team2, 1, ZonedDateTime.now()));
+        Game selectedGame2 = gameService.create(new Game(team2, team3, 2, ZonedDateTime.now()));
+        Game selectedGame3 = gameService.create(new Game(team3, team4, 3, ZonedDateTime.now()));
+        WeeklyGameSelection weeklyGameSelection1 = weeklyGameSelectionService.create(new WeeklyGameSelection(user1, team2, selectedGame1));
+        WeeklyGameSelection weeklyGameSelection2 = weeklyGameSelectionService.create(new WeeklyGameSelection(user1, team3, selectedGame2));
+        WeeklyGameSelection weeklyGameSelection3 = weeklyGameSelectionService.create(new WeeklyGameSelection(user1, team4, selectedGame3));
+        assertNotNull(weeklyGameSelection1);
+        assertNotNull(weeklyGameSelection2);
+        assertNotNull(weeklyGameSelection3);
+    }
+
 }
